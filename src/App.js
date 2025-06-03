396d819dc6d2c07de11d2209e3108a465123f51b
duan150385duan <Lebron150385@163.com>
2025-06-03 22:21:28 +0800

    文件命名规范化

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ConfigProvider, Spin } from 'antd';
 import { BrowserRouter, useRoutes } from 'react-router-dom';
 import zhCN from 'antd/locale/zh_CN'; // 中文
 // import enUS from 'antd/locale/en_US'; // 英文
-import { Button, Flex } from 'antd';
+// import { Button, Flex } from 'antd';
 import routes from './routes/index';
 import { Suspense } from 'react';
 // import axios from 'axios';
